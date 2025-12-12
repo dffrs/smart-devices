8fc018f347f6d5c80cb95932c8ded9247f063d5c
Daniel Rodrigues <daniel.rodrigues@nokia.com>
2025-12-12 11:57:54 +0000

    feat: CLI for get status & methods

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	internal "smart-devices/internal/shelly"
 	outlet "smart-devices/internal/shelly/outlet"
 
 	"github.com/joho/godotenv"
@@ -14,6 +15,9 @@ const (
 	timeout = 10 // seconds
 	turnOn  = "on"
 	turnOff = "off"
+
+	getStatus  = "status"
+	getMethods = "methods"
 )
 
 func main() {
@@ -22,14 +26,39 @@ func main() {
 		log.Fatalf("Failed to load .env file\n%v", err)
 	}
 
-	state := flag.String("turn", turnOn, "Turn outlet on & off")
+	state := flag.String("turn", "", "Turn outlet on & off")
+	status := flag.String("get", "", "Get Status report from outlet")
 	flag.Parse()
 
 	host := os.Getenv("HOST")
-
 	outlet := outlet.NewShelly(host, timeout)
 
-	isOn := *state != turnOff
+	// TODO: clean me up
+	switch true {
 
-	outlet.Turn(isOn)
+	// deal with get status/methods options
+	case *status != "":
+		switch *status {
+		case getStatus:
+			output := outlet.GetStatus()
+			internal.PrettyPrint(output)
+		case getMethods:
+			output := outlet.ListMethods()
+			internal.PrettyPrint(output)
+		}
+
+	// deal with turn on/off option
+	case *state != "":
+		var isOn bool
+		switch *state {
+		case turnOn:
+			isOn = true
+		case turnOff:
+			isOn = false
+		default:
+			log.Fatalf("Unrecognized option for turn option\n")
+		}
+
+		outlet.Turn(isOn)
+	}
 }
