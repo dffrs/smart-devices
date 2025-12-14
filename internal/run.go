3fdcdbf79fdbeb3a2f7d0dc21a3c8018dcad915e
dffrs <dffrs@icloud.com>
2025-12-14 16:13:53 +0000

    refactor: cleaned up Run func

--- internal/run.go
+++ internal/run.go
@@ -14,6 +14,51 @@ const (
 	getMethods = "methods"
 )
 
+func dealWithStatus(status *string, outlet *shelly.Outlet) error {
+	var output any
+	var error error
+
+	// TODO: getStatus and listMethods should return errors
+	switch *status {
+	case getStatus:
+		output = outlet.GetStatus()
+	case getMethods:
+		output = outlet.ListMethods()
+	default:
+		error = errors.New("unrecognized option for get option")
+	}
+
+	if error != nil {
+		return error
+	}
+
+	PrettyPrint(output)
+
+	return nil
+}
+
+func dealWithState(state *string, outlet *shelly.Outlet) error {
+	var isOn bool
+	var err error
+
+	switch *state {
+	case turnOn:
+		isOn = true
+	case turnOff:
+		isOn = false
+	default:
+		err = errors.New("unrecognized option for turn option")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	outlet.Turn(isOn)
+
+	return nil
+}
+
 func Run(outlet *shelly.Outlet) error {
 	state, status := LoadFlags()
 
@@ -21,30 +66,17 @@ func Run(outlet *shelly.Outlet) error {
 
 	// deal with get status/methods options
 	case *status != "":
-		switch *status {
-		case getStatus:
-			output := outlet.GetStatus()
-			PrettyPrint(output)
-		case getMethods:
-			output := outlet.ListMethods()
-			PrettyPrint(output)
-		default:
-			return errors.New("unrecognized option for get option")
+		err := dealWithStatus(status, outlet)
+		if err != nil {
+			return err
 		}
 
 	// deal with turn on/off option
 	case *state != "":
-		var isOn bool
-		switch *state {
-		case turnOn:
-			isOn = true
-		case turnOff:
-			isOn = false
-		default:
-			return errors.New("unrecognized option for turn option")
+		err := dealWithState(state, outlet)
+		if err != nil {
+			return err
 		}
-
-		outlet.Turn(isOn)
 	}
 
 	return nil
